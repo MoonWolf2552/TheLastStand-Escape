5baccbdb2adb7c194cb0dbad1a8e2493fcb7da7e
MoonWolf2552 <iliyaklassgg@gmail.com>
2024-04-21 10:11:17 +0300

    Ver 0.1

--- Assets/Scripts/Starter.cs
+++ Assets/Scripts/Starter.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class Starter : MonoBehaviour
 {
+    [SerializeField] private bool _titres;
+    
     [ContextMenu("Start")]
     void Start()
     {
-        Player.Instance.MoveToSpawn();
-        StartCoroutine(Player.Instance.ScreenRemove());
-        EnemyCounter.Instance.Count();
+        if (!_titres)
+        {
+            Player.Instance.MoveToSpawn();
+            StartCoroutine(Player.Instance.ScreenRemove());
+            EnemyCounter.Instance.Count();
+        }
+        else
+        {
+            StartCoroutine(ScreenRemove());
+        }
+    }
+    
+    private IEnumerator ScreenRemove()
+    {
+        GameManager.Instance.BlackScreen.GetComponent<Animator>().SetTrigger("Hide");
+        yield return new WaitForSeconds(0.25f);
+        GameManager.Instance.BlackScreen.SetActive(false);
     }
 }
